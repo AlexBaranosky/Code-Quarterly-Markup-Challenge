26997aa11dc8fa41a7c48e21bab7f082bc9617a0
Alex Baranosky <alexander.baranosky@gmail.com>
2010-09-06 18:39:05 -0400

    made acceptance test file 06 pass.

--- src/facts/xmltransformationfacts.clj
+++ src/facts/xmltransformationfacts.clj
@@ -17,3 +17,8 @@
   (to-xml (body [(p "This is paragraph number one.") (p "This is paragraph number two.")]))
   =>
   "<body><p>This is paragraph number one.</p><p>This is paragraph number two.</p></body>")
+
+(fact
+  (to-xml (body (h1 "This is a top level header")))
+  =>
+  "<body><h1>This is a top level header</h1></body>")
